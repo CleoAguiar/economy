be6e4de95822befd9a1ddddbdc12fb28cd4e4515
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-21 08:55:22 -0300

    feat: manipulating html with jquery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
     $('.dinheiro').mask('#.##0,00', {reverse: true});
+    $('p:first').click(function(){
+        $('h2').show();
+        const currency = $('.dinheiro').val();
+        const val = parseFloat($('.dinheiro').mask('0.00').val());
+        $('.investment h3').text(val * 0.35);
+        // $('.fixed h3').text(val * 0.50);
+        // $('.study h3').text(val * 0.05);
+        // $('.sporadic h3').text(val * 0.10);
+
+    });    
 });
