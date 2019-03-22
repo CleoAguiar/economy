95d13a5c9fcad38e84d29c070f61aa55318dd905
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-22 08:35:07 -0300

    feat: add reset button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+    $('input').attr({
+        'placeholder': '1000.00',
+        'value': ''
+    });
+    $('h2').hide();
+    $('h3').hide();
+
     $('.dinheiro').mask('#.##0,00', {reverse: true});
+
     $('#calc').click(function(){
+        $('h3').show();
         $('h2').show();
         const currency = $('.dinheiro').val();
         const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
@@ -11,6 +20,7 @@ $(document).ready(function(){
 
     });   
     $('#reset').click(function(){
+        location.reload(true);
         $('h2').hide();
         $('h3').hide();
     });
