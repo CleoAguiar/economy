d9c58b9e78d5ff399b3a91736f4ca2adaace4657
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-22 13:03:48 -0300

    style: add boostrap

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
-    $('input').attr({
-        'placeholder': '1000.00',
-        'value': ''
+(function($){
+    $(document).ready(function(){
+        $('input').attr({
+            'placeholder': '1000.00',
+            'value': ''
+        });
+        $('h2').hide().addClass('hidden');
+        $('h3').hide().addClass('hidden');
+
+        $('.dinheiro').mask('#.##0,00', {reverse: true});
+
+        $('#calc').click(function(){
+            $('#calc').attr('disabled', true);
+            $('h3').show().removeAttr('hidden');
+            $('h2').show().removeAttr('hidden');
+            const currency = $('.dinheiro').val();
+            const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
+            $('.investment h3').text((val * 0.35).toFixed(2));
+            $('.fixed h3').text((val * 0.50).toFixed(2));
+            $('.study h3').text((val * 0.05).toFixed(2));
+            $('.sporadic h3').text((val * 0.10).toFixed(2));
+
+        });   
+        $('#reset').click(function(){
+            location.reload(true);
+            $('h2').hide().addClass('hidden');
+            $('h3').hide().addClass('hidden');
+        });
     });
-    $('h2').hide();
-    $('h3').hide();
+})(jQuery);
 
-    $('.dinheiro').mask('#.##0,00', {reverse: true});
-
-    $('#calc').click(function(){
-        $('#calc').attr('disabled', true);
-        $('h3').show();
-        $('h2').show();
-        const currency = $('.dinheiro').val();
-        const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
-        $('.investment h3').text((val * 0.35).toFixed(2));
-        $('.fixed h3').text((val * 0.50).toFixed(2));
-        $('.study h3').text((val * 0.05).toFixed(2));
-        $('.sporadic h3').text((val * 0.10).toFixed(2));
-
-    });   
-    $('#reset').click(function(){
-        location.reload(true);
-        $('h2').hide();
-        $('h3').hide();
-    });
-});
