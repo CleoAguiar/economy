c4f2b0a40fa046eb604bae228969856d48045c7c
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-22 09:59:25 -0300

    fix: fixed round 2

--- js/index.js
+++ js/index.js
@@ -9,14 +9,15 @@ $(document).ready(function(){
     $('.dinheiro').mask('#.##0,00', {reverse: true});
 
     $('#calc').click(function(){
+        $('#calc').attr('disabled', true);
         $('h3').show();
         $('h2').show();
         const currency = $('.dinheiro').val();
         const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
-        $('.investment h3').text(val * 0.35);
-        $('.fixed h3').text(val * 0.50);
-        $('.study h3').text(val * 0.05);
-        $('.sporadic h3').text(val * 0.10);
+        $('.investment h3').text((val * 0.35).toFixed(2));
+        $('.fixed h3').text((val * 0.50).toFixed(2));
+        $('.study h3').text((val * 0.05).toFixed(2));
+        $('.sporadic h3').text((val * 0.10).toFixed(2));
 
     });   
     $('#reset').click(function(){
