165244363b95a86e1a4457f1bdf560f86b0c52ac
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-22 08:38:29 -0300

    feat: show result h3

--- js/index.js
+++ js/index.js
@@ -14,9 +14,9 @@ $(document).ready(function(){
         const currency = $('.dinheiro').val();
         const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
         $('.investment h3').text(val * 0.35);
-        // $('.fixed h3').text(val * 0.50);
-        // $('.study h3').text(val * 0.05);
-        // $('.sporadic h3').text(val * 0.10);
+        $('.fixed h3').text(val * 0.50);
+        $('.study h3').text(val * 0.05);
+        $('.sporadic h3').text(val * 0.10);
 
     });   
     $('#reset').click(function(){
