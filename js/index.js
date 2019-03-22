891e31a9787e89e95fad5ade5d3a3f4120723188
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-22 07:18:44 -0300

    feat: add button calc and reset

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
     $('.dinheiro').mask('#.##0,00', {reverse: true});
-    $('p:first').click(function(){
+    $('#calc').click(function(){
         $('h2').show();
         const currency = $('.dinheiro').val();
-        const val = parseFloat($('.dinheiro').mask('0.00').val());
+        const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
         $('.investment h3').text(val * 0.35);
         // $('.fixed h3').text(val * 0.50);
         // $('.study h3').text(val * 0.05);
         // $('.sporadic h3').text(val * 0.10);
 
-    });    
+    });   
+    $('#reset').click(function(){
+        $('h2').hide();
+        $('h3').hide();
+    });
 });
