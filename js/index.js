34b9fb3ea35a39bcca1ad9629844b4aa297ba280
Cleo Aguiar <cleotaisas@gmail.com>
2019-03-26 11:30:12 -0300

    style: remove button attr disabled

--- js/index.js
+++ js/index.js
@@ -8,10 +8,11 @@
         $('.dinheiro').mask('#.##0,00', {reverse: true});
 
         $('#calc').click(function(){
-            $('#calc').attr('disabled', true);
             $('div').removeClass('invisible');
+            
             const currency = $('.dinheiro').val();
             const val = parseFloat($('.dinheiro').mask('###0.00', {reverse: true}).val());
+            
             $('.investment h4').text('R$ ' + (val * 0.35).toFixed(2));
             $('.fixed h4').text('R$ ' + (val * 0.50).toFixed(2));
             $('.study h4').text('R$ ' + (val * 0.05).toFixed(2));
